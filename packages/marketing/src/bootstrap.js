f96ab56b5c1350c62f45b84aa0e1076cb2055ad6
sebastian mihalache <sebastian.mihalache@mindera.com>
2021-02-24 18:05:40 +0200

    scaffold basic marketing page

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,9 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./App";
 
-const mount = (el) => {
-  ReactDOM.render(<h1>Hi there</h1>, el);
-};
+const mount = (el) => ReactDOM.render(<App />, el);
 
 if (process.env.NODE_ENV === "development") {
   const devRoot = document.querySelector("#_marketing_dev_root");
